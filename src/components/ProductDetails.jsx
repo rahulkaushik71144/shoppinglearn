1ac54d381c4fb6df6fc3a7de9ee3080e573b61b9
Rahul Kaushik <rahulkaushik71144@gmail.com>
2024-09-10 16:39:42 +0530

    Made some changes

---
+++ src/components/ProductDetails.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+const products = [
+{id:1 , name:"Apple", price: 1.4, Description: "An apple a day keeps a doctor away, warning! not for docs"},
+{id:2, name:"Banana", price: 6.8, Description: "A big long thick banana"},
+{id:3, name:"Cherry", price: 9.32, Description: "Cherry on top"},
+];
+const ProductDetails = () =>
+{
+    
+}
+
+
