72dede9cf45723f02699c3dda603348c08a585f8
Rahul Kaushik <rahulkaushik71144@gmail.com>
2024-09-10 16:17:35 +0530

    Modified the code

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import {Link} from 'react';
 const Header = () => {
     return(
        
         <header>
             <h1> Simple shopping site made by Rahul Kaushik</h1>
             <nav>
-                <a href="#"> Home</a>
-                <a href="#"> Shopping cart</a>
+                <Link to ="/"> Home</Link>
+                <Link to ="Cart"> Shopping cart</Link>
+                <Link to ="Products"> Products</Link>
+            
+            
             </nav>
         </header>
     
