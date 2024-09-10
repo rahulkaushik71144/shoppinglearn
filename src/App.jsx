d3ce1484f711df863b91197c80f9ca3e2e24a91f
Rahul Kaushik <rahulkaushik71144@gmail.com>
2024-09-10 13:35:33 +0530

    Changes made

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react'
 import Header from './components/Header.jsx'
 import '/home/rahul/Desktop/shoppinglearn/App.css';
-
 import ProductList from './components/ProductList.jsx';
+import Cart from './components/Cart.jsx';
 const App = () =>
 {
   const [cartItems, setCartItems] = useState([]);
--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -5,7 +5,19 @@ const Cart = ({cartItems})=>
     <div>
         <h2> Shopping Cart</h2>
         {cartItems.lenght===0?(<p> your cart is empty</p>):
+        (
+            <ul>
+                {
+                    cartItems.map((item, index)=>(
+                        <li key = {index}>
+                            {item.name} - ${item.price.ToFixed(2)}
+                        </li>
+                    ))
+                }
+            </ul>
+        )
         (<p></p>)}
     </div>
- )
-}
+ );
+};
+export default Cart;
