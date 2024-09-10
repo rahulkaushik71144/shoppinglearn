2a2110c86dd0b550a9d31f65ea5aaf98ed1b5b23
Rahul Kaushik <rahulkaushik71144@gmail.com>
2024-09-10 14:20:55 +0530

    fixed some typos

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,23 +1,23 @@
-// src/components/Cart.jsx
-import React from "react";
-
-const Cart = ({ cartItems }) => {
-  return (
+import React from 'react';
+const Cart = ({cartItems})=>
+{
+ return(
     <div>
-      <h2>Shopping Cart</h2>
-      {cartItems.length === 0 ? (
-        <p>Your cart is empty.</p>
-      ) : (
-        <ul>
-          {cartItems.map((item, index) => (
-            <li key={index}>
-              {item.name} - ${item.price.toFixed(2)}
-            </li>
-          ))}
-        </ul>
-      )}
+        <h2> Shopping Cart</h2>
+        {cartItems.length===0?(<p> your cart is empty</p>):
+        (
+            <ul>
+                {
+                    cartItems.map((item, index)=>(
+                        <li key = {index}>
+                            {item.name} - ${item.price.toFixed(2)}
+                        </li>
+                    ))
+                }
+            </ul>
+        )
+        }
     </div>
-  );
+ );
 };
-
 export default Cart;
