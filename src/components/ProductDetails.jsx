f4512544a8a83d76bc9782c60efb85e72631ff74
Rahul Kaushik <rahulkaushik71144@gmail.com>
2024-09-10 17:28:27 +0530

    Done with day 2

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 const products = [
-{id:1 , name:"Apple", price: 1.4, Description: "An apple a day keeps a doctor away, warning! not for docs"},
-{id:2, name:"Banana", price: 6.8, Description: "A big long thick banana"},
-{id:3, name:"Cherry", price: 9.32, Description: "Cherry on top"},
+{id:1 , name:"Apple", price: 1.4, description: "An apple a day keeps a doctor away, warning! not for docs"},
+{id:2, name:"Banana", price: 6.8, description: "A big long thick banana"},
+{id:3, name:"Cherry", price: 9.32,description: "Cherry on top"},
 ];
 const ProductDetails = () =>
 {
     const {productId} = useParams();
-    product = products.find((p)=>p.id===parseInt(productId));
+   const  product = products.find((p)=>p.id===parseInt(productId));
     if (!product){
         return <p>Product not found</p>
 
@@ -25,5 +25,6 @@ const ProductDetails = () =>
     );
 
 };
+export default ProductDetails;
 
 
